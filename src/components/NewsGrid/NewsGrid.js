ae54588de1adc0dc47caa5f64fb84cdba1e70eb6
Advait Junnarkar <advait.ju@gmail.com>
2017-07-20 21:00:18 +1000

    Added Ged Github ribbon. Article published dates displaying again.

--- src/components/NewsGrid/NewsGrid.js
+++ src/components/NewsGrid/NewsGrid.js
@@ -10,7 +10,7 @@ class NewsGrid extends Component {
 		const articles = this.props.articles.map(list => {
 			return list.articles.map((article, index) =>
 				<div className={`col-sm-4 col-md-3 col-lg-2 ${article.show && list.show ? '' : 'hide'}`}>
-					<Article key={index} url={article.url} img={article.urlToImage} title={article.title} author={list.source.charAt(0).toUpperCase() + list.source.replace(/-/g, ' ').slice(1)} published={article.publishedAt === !undefined ? article.publishedAt.slice(0,9) : '' } snippet={article.description.substring(0,150) + '[...continue]'} />
+					<Article key={index} url={article.url} img={article.urlToImage} title={article.title} author={list.source.charAt(0).toUpperCase() + list.source.replace(/-/g, ' ').slice(1)} published={article.publishedAt !== undefined ? article.publishedAt.slice(0,9).replace(/-/g, '/') : '' } snippet={article.description.substring(0,150) + '[...continue]'} />
 				</div>
 			);
 		});
